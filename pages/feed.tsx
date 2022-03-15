7d4749cce9b46dc077656c08fe1f8bbed01188b2
Arman <arman@nytesoftware.com>
2022-03-15 07:01:56 -0700

    Fix feed

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -117,12 +117,11 @@ const Feed: NextPage = () => {
       <div>
         <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
 
-            {featuredIds && featuredIds.length > 0 && (
+            {featuredIds && restaurants && featuredIds.length > 0 && (
                 <>
                 <h1 className='text-3xl text-gray-900 font-semibold mb-4'>Featured Near You</h1>
                 <div className='grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8 mb-8'>
-                    {featuredIds.map((id) => <RestaurantCard key={id} restaurant={restaurants.find(r => r.id === id)}/>)
-                    }
+                    {restaurants.filter(r => featuredIds.includes(r.id)).map(r => <RestaurantCard key={r.id} restaurant={r} />)}
                 </div>
                 </>
             )}
